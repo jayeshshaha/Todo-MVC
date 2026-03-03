8f4b2150a6ec7f5be2dfd08584f9ee2fcad5c993
Jayesh Shaha <jayeshshah4740@gmail.com>
2026-03-03 23:29:34 +0530

    Add project files.

---
+++ Todo.Web/Controllers/TodosController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Todo.Web.Controllers
+{
+    public class TodosController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
